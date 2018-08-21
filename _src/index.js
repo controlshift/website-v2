b7e4785e05558cdb3113deeb56d62d2b9edf91fb
James Rees <jarees@gmail.com>
2018-08-21 17:10:37 +0200

    Add status indicator

--- _src/index.js
+++ _src/index.js
@@ -1,4 +1,13 @@
 // CSS and SASS files
 import 'bootstrap';
-
+import $ from 'jquery';
 import './index.scss';
+
+$.getJSON('https://cfyy3d57ng36.statuspage.io/api/v2/status.json', (data) => {
+  $(() => {
+    $('.status-description').text(data.status.description);
+    $('.status-indicator span').replaceWith($(`<i class="indicator ${data.status.indicator} fa fa-circle"></i>`));
+  });
+});
+
+console.log('frist');
