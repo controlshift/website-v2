3af1e0a93cd6a501c5718cd7b422e06768fbf442
Nathan Woodhull <woodhull@gmail.com>
2018-08-21 20:03:12 -0400

    code style jQuery instead of $

--- _src/index.js
+++ _src/index.js
@@ -1,13 +1,11 @@
 // CSS and SASS files
 import 'bootstrap';
-import $ from 'jquery';
+import jQuery from 'jquery';
 import './index.scss';
 
-$.getJSON('https://cfyy3d57ng36.statuspage.io/api/v2/status.json', (data) => {
-  $(() => {
-    $('.status-description').text(data.status.description);
-    $('.status-indicator span').replaceWith($(`<i class="indicator ${data.status.indicator} fa fa-circle"></i>`));
+jQuery.getJSON('https://cfyy3d57ng36.statuspage.io/api/v2/status.json', (data) => {
+  jQuery(() => {
+    jQuery('.status-description').text(data.status.description);
+    jQuery('.status-indicator span').replaceWith(jQuery(`<i class="indicator ${data.status.indicator} fa fa-circle"></i>`));
   });
 });
-
-console.log('frist');
