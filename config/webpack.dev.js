511cca434c8c2b25dabea4415e112d2c25c14400
jamesr2323 <jarees@gmail.com>
2018-08-23 17:07:51 +0100

    WIP Respond QA

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const Merge = require('webpack-merge');
 const CommonConfig = require('./webpack.common.js');
 const path = require('path');
@@ -8,37 +9,37 @@ module.exports = Merge(CommonConfig, {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve('assets'),
-    publicPath: '/assets/'
+    publicPath: '/assets/',
   },
   devtool: 'inline-source-map',
   plugins: [
     new BrowserSyncPlugin(
       {
-        host: '0.0.0.0',
-        port: 5000,
-        proxy: 'http://localhost:5001',
-        files: ["_site", "_src", "_layouts"]
+        host: 'localhost',
+        port: 3000,
+        proxy: 'http://localhost:8080',
+        files: ['_site', '_src'],
       },
       {
-        reload: false
-      }
+        reload: false,
+      },
     ),
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
   ],
   module: {
     rules: [
       {
-				test: /\.js$/,
-				enforce: "pre",
-				exclude: /node_modules/,
-        loader: "eslint-loader",
+        test: /\.js$/,
+        enforce: 'pre',
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
       },
-    ]
+    ],
   },
   devServer: {
     contentBase: [
       path.resolve('_site'),
     ],
-    hot: true
-  }
+    hot: true,
+  },
 });
