8cea352831ce3c85c078bf49baa012cfd591b973
Nathan Woodhull <woodhull@gmail.com>
2018-09-22 16:57:08 -0400

    on click, open the zendesk widget instead of mailto:

--- _src/index.js
+++ _src/index.js
@@ -9,3 +9,11 @@ jQuery.getJSON('https://cfyy3d57ng36.statuspage.io/api/v2/status.json', (data) =
     jQuery('.status-indicator span').replaceWith(jQuery(`<i class="indicator ${data.status.indicator} fa fa-circle"></i>`));
   });
 });
+
+/* global zE */
+
+jQuery(() => {
+  jQuery('.lets-talk-link').on('click', () => {
+    zE.activate();
+  });
+});
