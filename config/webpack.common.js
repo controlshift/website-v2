7a43dff1371f94277bca33a47d40f4e50a5c3950
Nathan Woodhull <woodhull@gmail.com>
2021-01-25 10:42:16 -0500

    Cleanup @babel/preset-env configuration to make it clear we are using the browserlist defaults.

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -52,7 +52,9 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
+            presets: [
+              ['@babel/preset-env', { targets: 'defaults' }]
+            ]
           }
         }
       },
