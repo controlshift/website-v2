8de7fe277a9029da6d1e2655c3bb8dfbc1118c42
Nathan Woodhull <nathan@controlshiftlabs.com>
2021-02-03 15:05:25 -0500

    Subscribe function (#28)
    * Add Hello World netlify function.
    
    * working netlify mailchimp subscribe function.
    
    * Working mailchimp subscribe netlify function with recaptcha, client and server side validation, and help messaging. Also adds polyfills and corrects babel configuration.
    
    * Clean up recaptcha messaging.
    
    * Do not let clients know if the email address is currently subscribed as a privacy improvement.
    
    * make error message more easily readable against a dark background.
    
    * Remove debug output
    
    * Fix PR feedback

---
+++ functions/subscribe.js
@@ -0,0 +1,51 @@
+const Mailchimp = require('mailchimp-api-v3');
+const fetch = require('node-fetch');
+const querystring = require('querystring');
+
+exports.handler = async function(event, context) {
+
+  // Only allow POST
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+  const params = querystring.parse(event.body);
+
+  const VERIFY_URL = `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_API_KEY}&response=${params['g-recaptcha-response']}`;
+
+  const MAILCHIMP_LIST_ID = 'b4d8624172'
+
+  const recaptchaResponse = await fetch(VERIFY_URL, { method: 'POST' })
+  const recaptchaData = await recaptchaResponse.json()
+  let result;
+
+  if(recaptchaData.success) {
+    let mailchimp = new Mailchimp(process.env.MAILCHIMP_API_KEY);
+    try {
+      const mailchimpResult = await mailchimp.request({
+        method : 'post',
+        path : `/lists/${MAILCHIMP_LIST_ID}/members`,
+        body : {
+          email_address : params.email,
+          status : 'subscribed'
+        }
+      })
+      console.log(mailchimpResult)
+      result = {success: true, message: `${params.email} has been subscribed to the ControlShift mailing list.`};
+    } catch(err) {
+      const message = err.response.body.detail
+      if (message.match(/is already a list member/)) {
+        result = {success: true, message: `${params.email} has been subscribed to the ControlShift mailing list.`};
+      } else {
+        result = {success: false, message: message};
+      }
+    }
+  } else {
+    result = {success: false, message: 'reCaptcha rejected submission'}
+  }
+
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(result)
+  };
+}
