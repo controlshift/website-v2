9d6c10c6ba5c85eb5091b501a32917156d04b0f2
Grey Moore <grey@controlshiftlabs.com>
2022-12-08 16:06:10 -0500

    footer: replace status widget with link to status page

--- _src/index.js
+++ _src/index.js
@@ -14,13 +14,6 @@ import netlifyIdentity from 'netlify-identity-widget'
 import { setupValidation } from './validation'
 import { setupHamburger } from './hamburger'
 
-jQuery.getJSON('https://cfyy3d57ng36.statuspage.io/api/v2/status.json', (data) => {
-  jQuery(() => {
-    jQuery('.status-description').text(data.status.description)
-    jQuery('.status-indicator span').replaceWith(jQuery(`<i class="indicator ${data.status.indicator} fa fa-circle"></i>`))
-  })
-})
-
 jQuery(() => {
   setupHamburger()
   setupValidation()
