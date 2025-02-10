1cc59f90ef9fa0a0220303b5d8e5cb4f7a5c06dd
anero <anero79@gmail.com>
2025-02-10 17:40:07 -0300

    Read user's recaptcha response param from context argument

--- functions/subscribe.js
+++ functions/subscribe.js
@@ -2,7 +2,9 @@ import Mailchimp from 'mailchimp-api-v3';
 import fetch from 'node-fetch';
 import querystring from 'querystring';
 
-export default async (request, _context) => {
+export default async (request, context) => {
+
+  console.log(`Processing suscribe request.\nRequest: ${JSON.stringify(request)}\nContext: ${JSON.stringify(context)}`)
 
   // Only allow POST
   if (request.method !== "POST") {
@@ -12,9 +14,12 @@ export default async (request, _context) => {
   const VERIFY_URL = `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_API_KEY}&response=${params['g-recaptcha-response']}`;
   const MAILCHIMP_LIST_ID = 'b4d8624172'
 
+  console.log(`Performing reCaptcha verification.`)
   const recaptchaResponse = await fetch(VERIFY_URL, { method: 'POST' })
   const recaptchaData = await recaptchaResponse.json()
   let result;
+
+  console.log(`reCaptcha verification result: ${JSON.stringify(recaptchaData)}`)
 
   if(recaptchaData.success) {
     let mailchimp = new Mailchimp(process.env.MAILCHIMP_API_KEY);
@@ -27,9 +32,11 @@ export default async (request, _context) => {
           status : 'subscribed'
         }
       })
-      console.log(mailchimpResult)
+      console.log(`Maichimp result: ${JSON.stringify(mailchimpResult)}`)
       result = {success: true, message: `${params.email} has been subscribed to the ControlShift mailing list.`};
     } catch(err) {
+      console.log(`Error creating subscription on Maichimp: ${JSON.stringify(err)}`)
+
       const message = err.response.body.detail
       if (message.match(/is already a list member/)) {
         result = {success: true, message: `${params.email} has been subscribed to the ControlShift mailing list.`};
